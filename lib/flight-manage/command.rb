721cb106384843ab5c09cb9bd94fdea78457840f
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-18 15:56:27 +0000

    Create command superclass

---
+++ lib/flight-manage/command.rb
@@ -0,0 +1,46 @@
+# ==============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of Flight Manage.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-software.com.
+#
+# Flight Manage is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Manage. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Manage, please visit:
+# https://github.com/openflighthpc/flight-manage
+# ==============================================================================
+
+require 'ostruct'
+
+module FlightManage
+  class Command
+    def initialize(argv, options)
+      @argv = argv.freeze
+      @options = OpenStruct.new(options.__hash__)
+    end
+
+    # this wrapper is here to later enable error handling &/ logging
+    def run!
+      run
+    end
+
+    def run
+      raise NotImplementedError
+    end
+  end
+end
