485a054dd751a0e4644cafc0bae90da145110ed2
DavidMarchant <davidwjmarchant@gmail.com>
2019-02-18 14:54:51 +0000

    Add commander command definitions
    Currently with `nil` as their actions

--- lib/flight-manage/cli.rb
+++ lib/flight-manage/cli.rb
@@ -17,5 +17,64 @@ module FlightManage
 
     #silent_trace!
 
+    class << self
+      def action(command, klass)
+        command.action do |args, options|
+          klass.new(args, options).run!
+        end
+      end
+
+      def cli_syntax(command, args_str = nil)
+        command.syntax = [
+          PROGRAM_NAME,
+          command.name,
+          args_str
+        ].compact.join(' ')
+      end
+    end
+
+    command :node do |c|
+      cli_syntax(c, 'SUBCOMMAND')
+      c.description = "Manage nodes"
+      c.configure_sub_command(self)
+    end
+
+    command :'node show' do |c|
+      cli_syntax(c, '[NODE]')
+      c.description = "Show exectution history on a node"
+      c.hidden = true
+      action(c, nil)
+    end
+
+    command :script do |c|
+      cli_syntax(c, 'SUBCOMMAND')
+      c.description = "Manage scripts"
+      c.configure_sub_command(self)
+    end
+
+    command :'script list' do |c|
+      cli_syntax(c)
+      c.description = "List available scripts"
+      c.hidden = true
+      action(c, nil)
+    end
+
+    command :'script show' do |c|
+      cli_syntax(c, 'SCRIPT')
+      c.description = "Show execution history of a script"
+      c.hidden = true
+      action(c, nil)
+    end
+
+    command :'script run' do |c|
+      cli_syntax(c, '[SCRIPT]')
+      c.description = "Execute scripts"
+      c.hidden = true
+      c.option '-r', '--role ROLE',
+        "Run all scripts with ROLE (and no STAGE unless --stage is passed)"
+      c.option '-s', '--stage',
+        "Run all scripts with STAGE (and no ROLE unless --role is passed)"
+      action(c, nil)
+    end
   end
 end
